38efe8775db8fd2e05614def06e4afa8a3fadd3d
BayuHarmono <bayuharmo55@gmail.com>
2024-10-31 17:00:22 +0700

    Update main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+function toogleDetail(e) {
+    const target = $(e.target)
+
+    if($(target).hasClass("active")) {
+        $(target).html("Tampilkan Info").removeClass("active")
+    } else {
+        $(target).html("Sembunyikan Info").addClass("active")
+    }
+
+
+
+    const item = $(target).parents(".about-exp-item")
+    const detail = $(item).children(".about-exp-item-detail")
+
+    $(detail).slideToggle()
+}
+
+// function onFormSubmit(e) {
+//     e.preventDefault()
+//     const email = $("#inp_email")
+//     const subject = $("#inp_subjek")
+//     const message = $("#inp_message")
+
+//     if(!$(email).val()) {
+//         alert("Email is required")
+//     } else if (!$(subject).val()) {
+//         alert("Subjek is required")
+//     } else if (!$(message).val()) {
+//         alert("Message is required")
+//     } else {
+//         $(email).val("")
+//         $(subject).val("")
+//         $(message).val("")
+//         alert("Form Submitted")
+//     }
+// }
